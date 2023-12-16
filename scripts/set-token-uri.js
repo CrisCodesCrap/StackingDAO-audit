3c93c0418b2af2ed8493ecf63c4bdea87932084a
Philip De Smedt <philip.desmedt@gmail.com>
2023-12-16 17:32:14 +0100

    misc: add script to set token URI

---
+++ scripts/set-token-uri.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const CONTRACT_NAME = 'ststx-token';
+const FUNCTION_NAME = 'set-token-uri';
+const tx = require('@stacks/transactions');
+const utils = require('./utils');
+const network = utils.resolveNetwork();
+const BN = require('bn.js');
+
+const txOptions = {
+  contractAddress: CONTRACT_ADDRESS,
+  contractName: CONTRACT_NAME,
+  functionName: FUNCTION_NAME,
+  functionArgs: [
+    tx.stringUtf8CV('https://stackingdao.com/ststx-token.json')
+  ],
+  fee: new BN(100000, 10),
+  senderKey: process.env.STACKS_PRIVATE_KEY,
+  postConditionMode: 1,
+  network
+};
+
+async function transact() {
+  const transaction = await tx.makeContractCall(txOptions);
+  const result = tx.broadcastTransaction(transaction, network);
+  await utils.processing(result, transaction.txid(), 0);
+};
+
+transact();
