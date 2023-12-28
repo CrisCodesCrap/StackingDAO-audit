aada42d2ec015236a156c7a550b3431f9642dd32
Philip De Smedt <philip.desmedt@gmail.com>
2023-12-28 12:20:39 +0100

    chore: script to increase stacker (#93)
    * chore: script to increase stacker
    
    * 3 stx fee
    
    ---------
    
    Co-authored-by: Niel Deckx <hello@nieldeckx.be>

---
+++ scripts/stacker-increase.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const tx = require('@stacks/transactions');
+const utils = require('./utils');
+const network = utils.resolveNetwork();
+const BN = require('bn.js');
+
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const CONTRACT_NAME = 'stacker-2';
+const FUNCTION_NAME = 'stack-increase';
+
+const txOptions = {
+  contractAddress: CONTRACT_ADDRESS,
+  contractName: CONTRACT_NAME,
+  functionName: FUNCTION_NAME,
+  functionArgs: [
+    tx.contractPrincipalCV(CONTRACT_ADDRESS, 'reserve-v1'),
+    tx.uintCV(40000 * 1000000)
+  ],
+  fee: new BN(3000000, 10),
+  senderKey: process.env.STACKS_PRIVATE_KEY,
+  postConditionMode: 1,
+  network
+};
+
+async function transact() {
+  const transaction = await tx.makeContractCall(txOptions);
+  const result = tx.broadcastTransaction(transaction, network);
+  await utils.processing(result, transaction.txid(), 0);
+};
+
+transact();
