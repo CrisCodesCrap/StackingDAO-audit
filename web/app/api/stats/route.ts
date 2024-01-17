650bdddff28181a7a4387c7f10b555d0b17ceab7
Niel Deckx <hello@nieldeckx.be>
2024-01-17 09:07:44 +0100

    fix: api cors issue (#134)
    * fix: api cors issue
    
    * fix network

--- web/app/api/stats/route.ts
+++ web/app/api/stats/route.ts
@@ -1,23 +1,29 @@
 import { NextResponse } from "next/server";
 import { callReadOnlyFunction } from '@stacks/transactions';
-import { coreApiUrl } from "@/app/common/utils";
+import { coreApiUrl, stacksNetwork } from "@/app/common/utils";
 
 const fetchTVL = async () => {
+  // Total STX
   const result: any = await callReadOnlyFunction({
     contractAddress: process.env.NEXT_PUBLIC_STSTX_ADDRESS!,
     contractName: 'reserve-v1',
     functionName: 'get-total-stx',
     functionArgs: [],
     senderAddress: process.env.NEXT_PUBLIC_STSTX_ADDRESS!,
+    network: stacksNetwork
   });
   const totalSTX = Number(result.value?.value) / 1000000;
 
-  const url = "https://api.coingecko.com/api/v3/simple/price?ids=blockstack&vs_currencies=usd&precision=6";
-  const response = await fetch(url, { credentials: 'omit' });
+  // Fetch STX price
+  const url = 'https://laozi1.bandchain.org/api/oracle/v1/request_prices?ask_count=16&min_count=10&symbols=STX';
+  const response = await fetch(url);
   const data = await response.json();
-  const priceSTX = data.blockstack.usd;
+  if (data['price_results']?.length > 0) {
+    const priceSTX = data['price_results'][0]['px'] / Number(data['price_results'][0]['multiplier']);
+    return priceSTX * totalSTX;
+  }
 
-  return priceSTX * totalSTX;
+  return 0;
 }
 
 const fetchPoX = async () => {
@@ -41,5 +47,12 @@ export async function GET() {
     pox_stx_locked: pox.current_cycle.stacked_ustx / 1000000,
     pox_avg_apy: 7.65,
     stackingdao_tvl: tvl 
-  }, { status: 200 });
+  }, { 
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    }
+  });
 }
