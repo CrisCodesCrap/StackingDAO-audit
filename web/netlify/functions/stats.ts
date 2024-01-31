e557e49a89d0f4f7c2e2a08f434e2c09289d500a
Niel Deckx <hello@nieldeckx.be>
2024-01-31 11:49:00 +0100

    feat: use netlify functions for api (#181)
    * feat: use netlify functions for api
    
    * chore: remove old api
    
    * fix: apy

--- web/netlify/functions/stats.ts
+++ web/netlify/functions/stats.ts
@@ -1,7 +1,6 @@
-import { NextResponse } from "next/server";
 import { callReadOnlyFunction } from '@stacks/transactions';
 import { coreApiUrl, stacksNetwork } from "@/app/common/utils";
-import { revalidatePath } from "next/cache";
+import { Handler } from "@netlify/functions";
 
 const fetchTVL = async () => {
   // Total STX
@@ -34,9 +33,7 @@ const fetchPoX = async () => {
   return data;
 }
 
-export async function GET() {
-  revalidatePath("/api/stats");
-
+export const handler: Handler = async (event, context) => {
   const [
     tvl,
     pox,
@@ -45,17 +42,16 @@ export async function GET() {
     fetchPoX(),
   ]);
 
-  return NextResponse.json({ 
+
+  const result = { 
     pox_cycle: pox.current_cycle.id,
     pox_stx_locked: pox.current_cycle.stacked_ustx / 1000000,
-    pox_avg_apy: 7.65,
+    pox_avg_apy: 6.35,
     stackingdao_tvl: tvl 
-  }, { 
-    status: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    }
-  });
+  }
+
+  return {
+    body: JSON.stringify(result),
+    statusCode: 200,
+  }
 }
