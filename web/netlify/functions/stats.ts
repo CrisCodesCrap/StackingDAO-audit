d644e545b3bf34796792b7824956919913a09e3b
Philip De Smedt <philip.desmedt@gmail.com>
2024-02-14 22:37:06 +0000

    feat: show PoX STX locked for Stacking DAO instead of global PoX

--- web/netlify/functions/stats.ts
+++ web/netlify/functions/stats.ts
@@ -14,13 +14,17 @@ const fetchTVL = async () => {
   });
   const totalSTX = Number(result.value?.value) / 1000000;
 
+  return totalSTX;
+}
+
+const fetchStxPrice = async () => {
   // Fetch STX price
   const url = 'https://laozi1.bandchain.org/api/oracle/v1/request_prices?ask_count=16&min_count=10&symbols=STX';
   const response = await fetch(url, { cache: 'no-store' });
   const data = await response.json();
   if (data['price_results']?.length > 0) {
     const priceSTX = data['price_results'][0]['px'] / Number(data['price_results'][0]['multiplier']);
-    return priceSTX * totalSTX;
+    return priceSTX;
   }
 
   return 0;
@@ -35,17 +39,19 @@ const fetchPoX = async () => {
 
 export const handler: Handler = async (event, context) => {
   const [
-    tvl,
+    stxPrice,
+    tvlInStx,
     pox,
   ] = await Promise.all([
+    fetchStxPrice(),
     fetchTVL(),
     fetchPoX(),
   ]);
-
+  const tvl = stxPrice * tvlInStx;
 
   const result = { 
     pox_cycle: pox.current_cycle.id,
-    pox_stx_locked: pox.current_cycle.stacked_ustx / 1000000,
+    pox_stx_locked: tvlInStx,
     pox_avg_apy: 6.35,
     stackingdao_tvl: tvl 
   }
