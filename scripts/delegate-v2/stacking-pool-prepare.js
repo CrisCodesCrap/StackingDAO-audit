64d304b66509f467ba41c273c080b6fed2b26fa8
Philip De Smedt <philip.desmedt@gmail.com>
2024-04-24 17:26:12 +0200

    chore: add stacking pool prepare script

---
+++ scripts/delegate-v2/stacking-pool-prepare.js
@@ -0,0 +1,27 @@
+require('dotenv').config({path: '../.env'});
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const CONTRACT_NAME = 'stacking-pool-v1';
+const FUNCTION_NAME = 'prepare-stacking-dao';
+const tx = require('@stacks/transactions');
+const utils = require('../utils');
+const network = utils.resolveNetwork();
+const BN = require('bn.js');
+
+const txOptions = {
+  contractAddress: CONTRACT_ADDRESS,
+  contractName: CONTRACT_NAME,
+  functionName: FUNCTION_NAME,
+  functionArgs: [],
+  fee: new BN(100000, 10),
+  senderKey: process.env.STACKS_PRIVATE_KEY,
+  postConditionMode: 1,
+  network
+};
+
+async function transact() {
+  const transaction = await tx.makeContractCall(txOptions);
+  const result = await tx.broadcastTransaction(transaction, network);
+  console.log(result);
+};
+
+transact();
