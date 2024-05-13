76010ddb1df0edf6da1a5bb41f4c6b3a4e33aaf2
Philip De Smedt <philip.desmedt@gmail.com>
2024-05-13 11:42:55 +0200

    chore: improve mining script to use core API url of quicknode

--- scripts/mining/get-bids.js
+++ scripts/mining/get-bids.js
@@ -16,17 +16,20 @@ function sleep(ms) {
 }
 
 // https://forum.stacks.org/t/proposed-sip-modify-coinbase-reward-per-block-and-halving-schedule/13182
+// const coreApiUrl = 'https://api.hiro.so';
+const coreApiUrl = "https://small-solemn-frost.stacks-mainnet.discover.quiknode.pro/deaf86bafdfbef850e40cdf5fa22c41cd447cdff"
+
 
 async function exec() {
   let offset = 0;
 
-  while (offset < 120) {
-    const url = `https://api.mainnet.hiro.so/extended/v2/blocks?limit=30&offset=${offset}`;
+  while (offset < 2100) {
+    const url = `${coreApiUrl}/extended/v2/blocks?limit=30&offset=${offset}`;
     const response = await fetch(url, { credentials: 'omit' });
     const data = await response.json();
 
     await asyncForEach(data['results'], async (result) => {
-      const blockUrl = `https://api.mainnet.hiro.so/extended/v2/blocks/${result.height}/transactions`;
+      const blockUrl = `${coreApiUrl}/extended/v2/blocks/${result.height}/transactions`;
       const response = await fetch(blockUrl, { credentials: 'omit' });
       const blockData = await response.json();
 
@@ -37,7 +40,7 @@ async function exec() {
       } else {
         miners[miner] = 1;
       }
-      await sleep(1000);
+      // await sleep(1000);
     });
 
     offset += 30;
